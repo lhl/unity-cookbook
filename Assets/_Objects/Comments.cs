ffc82ad5fd3f2ccb1fa9406bfb6f7656634bdbd1
lhl <lhl@randomfoo.net>
2015-11-06 03:02:06 +0800

    created Comment component for writing notes

---
+++ Assets/_Objects/Comments.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Comments : MonoBehaviour {
+
+    [TextArea(8, 100)]
+    public string comments;
+    public TextAreaAttribute text;
+}
