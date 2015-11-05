95500e1e6e99bdd669b974f5780f5a63b72eaf82
lhl <lhl@randomfoo.net>
2015-11-06 05:49:45 +0800

    Basic UI (button and event)

---
+++ Assets/_Objects/ui_button/ui_button_onclick.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ui_button_onclick : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	public void onclick () {
+        Debug.Log("click");
+	}
+}
